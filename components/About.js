56554ab459a7cfd39cc38703f20a2f30b9132cba
Garvit Singh <garvitsingh4575@gmail.com>
2023-11-22 07:42:36 +0530

    Escaped ' and "

--- components/About.js
+++ components/About.js
@@ -7,9 +7,9 @@ function About() {
         <h1 className="text-3xl font-medium pb-8">About iPhiPhi</h1>
         <div className="space-y-8 font-light">
           <p className="leading-relaxed">
-            Welcome to iPhiPi, started by Pankaj Joshi's his vision propels us
+            Welcome to iPhiPi, started by Pankaj Joshi&apos;s his vision propels us
             into the forefront of audio technology innovation. Fueled by an
-            unwavering passion for the next generation of "AI Smart" products.
+            unwavering passion for the next generation of &quot;AI Smart&quot; products.
             At iPhiPi, we specialize in creating intelligent chips seamlessly
             infused with AI capabilities, revolutionizing the way we listen and
             communicate.

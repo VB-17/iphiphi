58649a06e132d07a03cddf366d35b44786b47a3f
Garvit Singh <garvitsingh4575@gmail.com>
2023-11-28 16:09:26 +0530

    Made Contact work + Refactoring

--- components/Contact.js
+++ components/Contact.js
@@ -1,48 +1,61 @@
 "use client";
 
 import React, { useState } from "react";
-import { sendContactForm } from "@/lib/api";
 import toast from "react-hot-toast";
+// import nodemailer from "nodemailer";
 
 const initValues = { name: "", email: "", subject: "", message: "" };
-const initState = { isLoading: false, error: "", values: initValues };
+const touchedInit = { name: false, email: false, subject: false, message: false };
 
 function Contact() {
-  const [state, setState] = useState(initState);
-  const [touched, setTouched] = useState({});
-
-  const { values, isLoading, error } = state;
+  const [values, setValues] = useState(initValues);
+  const [touched, setTouched] = useState(touchedInit);
+  const [isLoading, setLoading] = useState(false);
 
   const onBlur = ({ target }) =>
     setTouched((prev) => ({ ...prev, [target.name]: true }));
 
   const handleChange = ({ target }) =>
-    setState((prev) => ({
+    setValues((prev) => ({
       ...prev,
-      values: {
-        ...prev.values,
-        [target.name]: target.value,
-      },
+      [target.name]: target.value
     }));
 
-    const onSubmit = async () => {
-    setState((prev) => ({
-      ...prev,
-      isLoading: true,
-    }));
-    try {
-      await sendContactForm(values);
-      setTouched({});
-      setState(initState);
-      toast.success("Email Sent");
-    } catch (error) {
-      setState((prev) => ({
-        ...prev,
-        isLoading: false,
-        error: error.message,
-      }));
-      toast.error("Some error occured");
-    }
+  const onSubmit = async () => {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail', 
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL, 
+      to: process.env.EMAIL,
+      subject: 'Test Email',
+      text: 'This is a test email sent using Nodemailer.',
+      html: '<p>This is a test email sent using <b>Nodemailer</b>.</p>', 
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Error occurred while sending email:', error);
+      } else {
+        console.log('Email sent:', info.response);
+      }
+    });
+
+    // setLoading(true)
+    // try {
+    //   await sendContactForm(values);
+    //   setTouched(touchedInit);
+    //   setValues(initState);
+    //   toast.success("Email Sent");
+    // } catch (error) {
+    //   setLoading(false);
+    //   toast.error("Some error occured");
+    // }
   };
   return (
     <div className="mx-auto py-20 max-w-screen-lx md:grid md:grid-cols-2 md:gap-16">
@@ -50,91 +63,95 @@ function Contact() {
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3803.1690037925537!2d78.12046517509106!3d17.594707796799785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcbefdc136bffbb%3A0x73414ff6594c9191!2sIndian%20Institute%20of%20Technology%20Hyderabad!5e0!3m2!1sen!2sin!4v1701157372966!5m2!1sen!2sin"
           className="w-full aspect-square border-0"
-          allowfullscreen=""
+          allowFullScreen=""
           loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
+          referrerPolicy="no-referrer-when-downgrade"
         ></iframe>
       </div>
-      <section class="bg-white">
-        <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-          <h2 class="mb-8 text-4xl tracking-tight font-semibold t text-gray-900">
+      <section className="bg-white">
+        <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+          <h2 className="mb-8 text-4xl tracking-tight font-semibold t text-gray-900">
             Contact Us
           </h2>
-          <form action="POST" class="space-y-8">
+          <form action="POST" className="space-y-8">
             <div>
               <label
-                for="name"
-                class="block mb-2 text-sm font-medium text-gray-900"
+                htmlFor="name"
+                className="block mb-2 text-sm font-medium text-gray-900"
               >
                 Your name
               </label>
               <input
                 type="name"
                 id="name"
+                name="name"
                 value={values.name}
                 onChange={handleChange}
                 onBlur={onBlur}
-                class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
                 placeholder="John Doe"
                 required
               />
             </div>
             <div>
               <label
-                for="email"
-                class="block mb-2 text-sm font-medium text-gray-900"
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900"
               >
                 Your email
               </label>
               <input
                 type="email"
                 id="email"
+                name="email"
                 value={values.email}
                 onChange={handleChange}
                 onBlur={onBlur}
-                class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
-                placeholder="name@flowbite.com"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
+                placeholder="john@example.com"
                 required
               />
             </div>
             <div>
               <label
-                for="subject"
-                class="block mb-2 text-sm font-medium text-gray-900"
+                htmlFor="subject"
+                className="block mb-2 text-sm font-medium text-gray-900"
               >
                 Subject
               </label>
               <input
                 type="text"
                 id="subject"
+                name="subject"
                 value={values.subject}
                 onChange={handleChange}
                 onBlur={onBlur}
-                class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500"
+                className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500"
                 placeholder="Let us know how we can help you"
                 required
               />
             </div>
-            <div class="sm:col-span-2">
+            <div className="sm:col-span-2">
               <label
-                for="message"
-                class="block mb-2 text-sm font-medium text-gray-900"
+                htmlFor="message"
+                className="block mb-2 text-sm font-medium text-gray-900"
               >
                 Your message
               </label>
               <textarea
                 id="message"
                 rows="6"
+                name="message"
                 value={values.message}
                 onChange={handleChange}
                 onBlur={onBlur}
-                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
                 placeholder="Leave a comment..."
               ></textarea>
             </div>
             <button
               type="submit"
-              isLoading={isLoading}
+              // isLoading={isLoading}
               disabled={
                 !values.name ||
                 !values.email ||
@@ -142,7 +159,11 @@ function Contact() {
                 !values.message
               }
               onClick={onSubmit}
-              class="py-4 px-5 text-sm font-medium text-center text-white rounded-lg bg-blue-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300"
+              className={`${!values.name ||
+                !values.email ||
+                !values.subject ||
+                !values.message
+                ? 'bg-blue-300' : 'bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-300'} py-4 px-5 text-sm font-medium text-center text-white rounded-lg sm:w-fit`}
             >
               Send message
             </button>
--- components/Footer.js
+++ components/Footer.js
@@ -5,12 +5,12 @@ function Footer() {
   return (
     <div className="bg-gray-300">
       <footer
-        class="bg-neutral-100 text-center text-neutral-600 dark:bg-neutral-600 dark:text-neutral-200 lg:text-left">
+        className="bg-neutral-100 text-center text-neutral-600 dark:bg-neutral-600 dark:text-neutral-200 lg:text-left">
         
-        <div class="max-w-screen-lx md:mx-auto mx-6 py-10 text-center md:text-left">
-          {/* <div class="grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-4 justify-between align-center"> */}
-          <div class="flex px-10 flex-col space-y-16 md:flex-row md:space-y-0 justify-between">
-            <div class="max-w-sm ">
+        <div className="max-w-screen-lx md:mx-auto mx-6 py-10 text-center md:text-left">
+          {/* <div className="grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-4 justify-between align-center"> */}
+          <div className="flex px-10 flex-col space-y-16 md:flex-row md:space-y-0 justify-between">
+            <div className="max-w-sm ">
               <div>
                 <Image
                   src="/logo-light.png"
@@ -24,32 +24,32 @@ function Footer() {
                 Intelligent Sensors for “AI Smart” Products. Next Generation of AI Smart Chips.
               </div>
             </div>
-            <div class="">
+            <div className="">
               <h6
-                class="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+                className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
                 Contact
               </h6>
-              <div class="mb-4 flex text-start">
+              <div className="mb-4 flex text-start">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
                   fill="currentColor"
-                  class="mr-3 w-5">
+                  className="mr-3 w-5">
                   <path
                     d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
                   <path
                     d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
                 </svg>
-                <div class="max-w-sm">
+                <div className="max-w-sm">
                   C5-104, Center for Innovation and Entrepreneurship <br/> IIIT Hyderabad <br/> Telangana - 500032
                 </div>
               </div>
-              <div class="mb-4 flex items-center justify-center md:justify-start">
+              <div className="mb-4 flex items-center justify-center md:justify-start">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
                   fill="currentColor"
-                  class="mr-3 w-5">
+                  className="mr-3 w-5">
                   <path
                     d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
                   <path
@@ -61,10 +61,10 @@ function Footer() {
           </div>
         </div>
 
-        <div class="bg-neutral-200 p-6 text-center dark:bg-neutral-700">
+        <div className="bg-neutral-200 p-6 text-center dark:bg-neutral-700">
           <span>© 2023 Copyright: </span>
           <a
-            class="font-semibold text-neutral-600 dark:text-neutral-400"
+            className="font-semibold text-neutral-600 dark:text-neutral-400"
             href="#"
           >IPhiPi</a
           >
